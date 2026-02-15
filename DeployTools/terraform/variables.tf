39407396ecf9ea4900ed17e8ca4ed6ee104e5787
Abhishek <abhishekburman54@gmail.com>
2026-02-15 10:09:23 +0530

    Update default values in variables.tf for AWS setup

--- DeployTools/terraform/variables.tf
+++ DeployTools/terraform/variables.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources into."
   type        = string
-  default     = "eu-north-1" # Set a default, but GitHub Actions will override via env
+  default     = "us-east-1" # Set a default, but GitHub Actions will override via env
 }
 
 variable "instance_type" {
@@ -14,14 +14,14 @@ variable "instance_type" {
 variable "ami_id" {
   description = "The AMI ID for the EC2 instance (e.g., Ubuntu, Amazon Linux 2)."
   type        = string
-  
-  default     = "ami-042b4708b1d05f512" # Canonical, Ubuntu, 24.04, amd64 noble image
+
+  default = "ami-0b6c6ebed2801a5cb" # Canonical, Ubuntu, 24.04, amd64 noble image
 }
 
 variable "key_pair_name" {
   description = "The name for the EC2 Key Pair."
   type        = string
-  default     = "my-ec2-keypair"
+  default     = "devops"
 }
 
 variable "environment" {
